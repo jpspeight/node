1b32ae758f79a774ddd489b55d9123ec15d99d39
James Speight <james.speight@pepperdine.edu>
2015-12-22 14:00:47 -0800

    Changed

--- mongoose.js
+++ mongoose.js
@@ -1,28 +1,34 @@
-	var mongoose = require('mongoose');
+	'use strict';
+	const mongoose = require('mongoose');
 	mongoose.connect('mongodb://localhost:27017/local');
+    const db = mongoose.connection;
 
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function (callback) {
- 		console.log("Successly connected to MongoDB!");
-	});
+    db.on('error', console.error.bind(console, 'connection error:'));
 
 
-	var nameSchema = mongoose.Schema({
-		firstName: String,
-		lastName: String,
-		age: Number
-	})
-	
-	var Name = mongoose.model('Name', nameSchema);
-	
-	var james = new Name({firstName : 'Canny', lastName : 'Speaky', age : 59});
-	
-	james.save(function(err, james){
-		if(err) return console.error(err);
-	});
-	
-	Name.find(function (err, james) {
+
+    db.once('open', function () {
+        console.log("Successly connected to MongoDB!");
+
+        let nameSchema = mongoose.Schema({ //create schema
+            firstName: String,
+            lastName: String,
+            age: Number
+        });
+
+        let Names = mongoose.model('Names', nameSchema); //create model with schema
+
+        let james = new Names({ //create instance
+            firstName : 'James',
+            lastName : 'Speight',
+            age : 59
+        });
+
+        james.save(function(err){
+            if(err) return console.error(err);
+        });
+    });
+
+	/*Name.find(function (err, james) {
   		if (err) return console.error(err);
-  		console.log(james)
-})
+  		console.log(james)*/
