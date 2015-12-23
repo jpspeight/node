72a41a6a3b7d0b50fecde55a86ef4d71c2e452c9
James Speight <james.speight@pepperdine.edu>
2015-12-22 19:52:43 -0800

    Database Add

--- mongoose.js
+++ mongoose.js
@@ -2,18 +2,18 @@
 	const mongoose = require('mongoose');
 	mongoose.connect('mongodb://localhost:27017/local');
     const db = mongoose.connection;
-
+    var n;
 
     const readline = require('readline'),
     rl = readline.createInterface(process.stdin, process.stdout);
-
-
-
 
     db.on('error', console.error.bind(console, 'connection error:'));
 
     db.once('open', function () {
         console.log("Successly connected to MongoDB!");
+        rl.setPrompt('Saving Entries> ');
+        rl.prompt();
+
 
         let nameSchema = mongoose.Schema({ //create schema
             firstName: String,
@@ -24,45 +24,36 @@
 
         var Names = mongoose.model('Names', nameSchema); //create model with schema
 
-        let james = new Names({ //create instance
+        let person = new Names({ //create instance
             firstName : 'annie',
-            lastName : 'biga',
-            age : 55
+            lastName : 'dallas',
+            age : 55,
+            id: 0
         });
 
-        james.pre('save', function(next){
-            let currentDate = new Date();
-            this.created_at = currentDate;
-            next();
-        });
-
-        james.save(function(err){
-            if(err) return console.error(err);
-            console.log('User saved successfully!')
-        });
+        rl.on('line', function(line) {
+            //console.log(line.trim());
+            rl.prompt();
 
 
-        process.stdin.on('data', function(data){
-            let response = data.toString().trim();
-            console.log(response);
+            //james.id++;
+            person.pre('save', function(next, done){
+                person.firstName = line.toString().trim();
+               /* this.id++;
+                console.log(this.id);*/
+                next();
+            });
 
-            //FIND ALL
-           /* Names.find(function (err, james) {
-                if (err) return console.error(err);
-                console.log(james);
-            });*/
-           /* Names.find({firstName:response }, function (err, user) {
-                if (err) return console.error(err);
-                console.log(user);
-            });*/
-
-          Names.find(1, function (err, user) {
-                if (err) return console.error(err);
-                console.log(user);
+            person.save(function(err){
+                if(err) return console.error(err);
+                console.log('User saved successfully!')
             });
 
         });
 
+        rl.on('close', function(){
+            process.exit(0);
+        });
 
     });
 
